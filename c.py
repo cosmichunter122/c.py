4c1d2fe50ebfaa442507c5fc95a8e0f7df0da574
cosmichunter122 <chethugreninja@gmail.com>
2026-06-18 21:53:08 +0530

    activity

---
+++ c.py
@@ -0,0 +1,15 @@
+print("🐱‍👤👀welcome to the fruit shop✨🐱‍💻")
+name =input("what is your name?")
+print("hello",name)
+apple=int(input("how many apples do you want"))
+banana=int(input("how many banana do you want"))
+total=apple+banana
+print("you brought",apple,"apple")
+print("you brought",banana,"banana")
+print("total fruits:",total)
+if total>=5:
+    print("🎁👀you get free sticker..✨🐱‍💻")
+else:
+    print("🎁👀buy more fruit to get more stickers..✨🐱‍💻")
+print("🎇🏆thank you for shopping!🍳🎇")
+
